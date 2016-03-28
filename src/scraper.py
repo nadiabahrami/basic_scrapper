8cefeb807feafe930e5846d7a5d5b6a37b6a175a
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-27 21:04:32 -0700

    Yeah I am going to bed.   I will come back to this assignment.

--- src/scraper.py
+++ src/scraper.py
@@ -6,4 +6,26 @@ from bs4 import BeautifulSoup
 """Note to TA.  I know there is nothing here.  I will come back to it later 
 to get full points."""
 
-http://info.kingcounty.gov/health/ehs/foodsafety/inspections/Results.aspx?Output=W&Business_Name=&Business_Address=&Longitude=&Latitude=&City=Seattle&Zip_Code=98101&Inspection_Type=All&Inspection_Start=&Inspection_End=&Inspection_Closed_Business=A&Violation_Points=&Violation_Red_Points=&Violation_Descr=&Fuzzy_Search=N&Sort=B
+
+# INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
+# INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+# INSPECTION_PARAMS = {
+#     'Output': 'W',
+#     'Business_Name': '',
+#     'Business_Address': '',
+#     'Longitude': '',
+#     'Latitude': '',
+#     'City': 'Seattle',
+#     'Zip_Code': '98101',
+#     'Inspection_Type': 'All',
+#     'Inspection_Start': '',
+#     'Inspection_End': '',
+#     'Inspection_Closed_Business': 'A',
+#     'Violation_Points': '',
+#     'Violation_Red_Points': '',
+#     'Violation_Descr': '',
+#     'Fuzzy_Search': 'N',
+#     'Sort': 'B'
+# }
+
+def get_inspection_page(parameters_go_here):
