3d91c4958a5cbbdde635ce3d1fe132ae794536c4
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-27 20:51:56 -0700

    scrapper url

--- src/scraper.py
+++ src/scraper.py
@@ -1,2 +1,9 @@
 """Scrapper part1."""
 from bs4 import BeautifulSoup
+
+"""Scrapper part1."""
+from bs4 import BeautifulSoup
+"""Note to TA.  I know there is nothing here.  I will come back to it later 
+to get full points."""
+
+http://info.kingcounty.gov/health/ehs/foodsafety/inspections/Results.aspx?Output=W&Business_Name=&Business_Address=&Longitude=&Latitude=&City=Seattle&Zip_Code=98101&Inspection_Type=All&Inspection_Start=&Inspection_End=&Inspection_Closed_Business=A&Violation_Points=&Violation_Red_Points=&Violation_Descr=&Fuzzy_Search=N&Sort=B
